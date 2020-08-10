2cfb5dcc87c78de37c08f23a6a9bb8315353a494
b0gdanp3trovic <bogdanlmaorofl@gmail.com>
2020-08-10 23:48:34 +0200

    .

--- server.py
+++ server.py
@@ -23,10 +23,7 @@ if(os.path.exists('./model/model.joblib')):
             print('Error loading model file.')
 elif('S3_BUCKET_NAME' in os.environ):
     print('Heroku environment detected.')
-    try:
-        model = joblib.load(configure_boto3())
-    except:
-        print('There was an error obtaining the file from S3.')
+    model = joblib.load(configure_boto3())
 
 
 @app.route('/', methods=['GET'])
